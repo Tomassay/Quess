c2d7abe647f75e75748a96b88e1dd803289fc302
Tomassay <levaytech@gmail.com>
2022-05-29 10:35:55 +0200

    'Quess_C#'

---
+++ Quess/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int number_to_quess = 99;
+
+Console.WriteLine("Quess a number: ");
+int player_number = Convert.ToInt32(Console.ReadLine());
+
+while(player_number != number_to_quess)
+{   
+    if(player_number > number_to_quess)
+    {
+        Console.WriteLine("too high");
+    }
+    else if (player_number < number_to_quess)
+    {
+        Console.WriteLine("too low");
+    }
+    Console.WriteLine("Quess another: ");
+    player_number = Convert.ToInt32(Console.ReadLine());
+
+}
+
+Console.WriteLine("You win");
